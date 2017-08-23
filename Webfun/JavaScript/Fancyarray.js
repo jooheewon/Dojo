784dc01dc54fbcf33de2974a42ed611acb90724e
Joy Joo <joyheewonjoo@gmail.com>
2017-08-23 16:35:31 -0400

    updating projects

--- Webfun/JavaScript/Fancyarray.js
+++
@@ -1,7 +0,0 @@
-var array = ["James", "Jill", "Jane", "Jack"];
-function fancyPrint(arr){
-  for (var i = 0; i < arr.length; i++) {
-  console.log(`${i} -> ${arr[i]}`)
-  }
-}
-fancyPrint(array)
--- Webfun/JavaScript/Range Print.js
+++
@@ -1,6 +0,0 @@
-function Rangeprint(start, end, iterate){
-for (var i = start; i < end; i += iterate) {
-  console.log(i)
-}
-}
-Rangeprint(2, 12, 4);
