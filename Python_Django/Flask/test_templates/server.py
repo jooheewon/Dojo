289ccf0ea3948de309ca18b6b5c3fcb3cfbf805e
Joy Joo <joyheewonjoo@gmail.com>
2017-10-09 09:45:05 -0400

    deleting folders

--- Python_Django/Flask/test_templates/server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request, redirect
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html', phrase="hello", times=5)
-
-app.run(debug=True)
